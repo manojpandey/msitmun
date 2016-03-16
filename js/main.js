f1431075d6a1257d5d0d7977d791a3ff0b2352a7
Ashish <ashishgupta.3197@gmail.com>
2016-03-16 13:18:30 +0530

    Add button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+jQuery(document).ready(function($){
+	var animate=false;
+	var navigationTrigger = $('.cd-nav-trigger'),
+	    transitionsNotSupported = ( $('.no-csstransitions').length > 0);
 
+	navigationTrigger.on('click', function(event){
+		event.preventDefault();
+		if(animate==false)
+		{
+			animate=true;
+			//alert('false');
+			if(navigationTrigger.hasClass('nav-visible'))
+			{
+				alert('has');
+				navigationTrigger.removeClass('.nav-visible');
+			}
+			else
+			{
+				//alert('has');
+				navigationTrigger.addClass('nav-visible');
+			}
+		}
+
+		if(transitionsNotSupported) animating = false;
+	});
+});
