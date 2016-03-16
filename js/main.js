53ba2c3a1218fe9ec14616258bc3838b33768e3e
Ashish <ashishgupta.3197@gmail.com>
2016-03-16 16:18:49 +0530

    Button animations

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
-jQuery(document).ready(function($){
-	var animate=false;
-	var navigationTrigger = $('.cd-nav-trigger'),
-	    transitionsNotSupported = ( $('.no-csstransitions').length > 0);
+jQuery(document).ready(function($) {
+    var animate = false;
+    var navigationTrigger = $('.cd-nav-trigger'),
+        transitionsNotSupported = ($('.no-csstransitions').length > 0);
 
-	navigationTrigger.on('click', function(event){
-		event.preventDefault();
-		if(animate==false)
-		{
-			animate=true;
-			//alert('false');
-			if(navigationTrigger.hasClass('nav-visible'))
-			{
-				alert('has');
-				navigationTrigger.removeClass('.nav-visible');
-			}
-			else
-			{
-				//alert('has');
-				navigationTrigger.addClass('nav-visible');
-			}
-		}
+    navigationTrigger.on('click', function(event) {
+        event.preventDefault();
 
-		if(transitionsNotSupported) animating = false;
-	});
+        if (animate == false) {
+            animate = true;
+            if (navigationTrigger.hasClass('nav-visible')) {
+                //alert('has');
+                navigationTrigger.removeClass('nav-visible');
+               
+            } else {
+                //alert('not has');
+                navigationTrigger.addClass('nav-visible');
+            }
+        }
+        if (transitionsNotSupported) animate = false;
+        animate=false;
+    });
 });
