68fde9cf76f0ecc2e8108f1f5e373a07db525b70
Ashish <ashishgupta.3197@gmail.com>
2016-03-17 21:12:03 +0530

    Position fix for safari

--- js/main.js
+++ js/main.js
@@ -14,5 +14,10 @@ jQuery(document).ready(function($) {
             });
         }
     });
-
+    $(function() {
+        if (navigator.userAgent.indexOf('Safari') != -1 &&
+            navigator.userAgent.indexOf('Chrome') == -1) {
+            $("body").addClass("safari");
+        }
+    });
 });
