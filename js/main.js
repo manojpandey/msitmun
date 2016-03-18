a83c252c2ade3d7da33bf9793ecef9b83bcc46e7
Ashish <ashishgupta.3197@gmail.com>
2016-03-18 21:33:51 +0530

    Add Info Pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 jQuery(document).ready(function($) {
-    var isLateralNavAnimating = false;
 
+    $('#fullpage').fullpage({
+        anchors: ['MainNav', 'Team','About'],
+        scrollingSpeed: 1000,
+        scrollOverflow: true,
+        touchSensitivity: 1
+    });
+
+    var isLateralNavAnimating = false;
     $('.nav-trigger').on('click', function(event) {
         event.preventDefault();
 
@@ -8,17 +15,22 @@ jQuery(document).ready(function($) {
             if ($(this).parents('.csstransitions').length > 0) isLateralNavAnimating = true;
 
             $('body').toggleClass('navigation-is-open');
-            // $('body').css()
             $('.navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
 
                 isLateralNavAnimating = false;
             });
         }
     });
+
     $(function() {
         if (navigator.userAgent.indexOf('Safari') != -1 &&
             navigator.userAgent.indexOf('Chrome') == -1) {
             $("body").addClass("safari");
         }
+    });
+
+    $('.navigation-wrapper').find('li').click(function() {
+
+        console.log($(this).index());
     });
 });
