9b8ee091d456e3e95db6a2d410b633de5ff8e0f8
Ashish <ashishgupta.3197@gmail.com>
2016-03-18 17:03:34 +0530

    Add layout page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 jQuery(document).ready(function($) {
     var isLateralNavAnimating = false;
 
-    $('.cd-nav-trigger').on('click', function(event) {
+    $('.nav-trigger').on('click', function(event) {
         event.preventDefault();
 
         if (!isLateralNavAnimating) {
             if ($(this).parents('.csstransitions').length > 0) isLateralNavAnimating = true;
 
             $('body').toggleClass('navigation-is-open');
-            $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+            // $('body').css()
+            $('.navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
 
                 isLateralNavAnimating = false;
             });
